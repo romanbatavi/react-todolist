0067fa05d14ca9908818ddbbb8d3a44e625aa818
Roman <73478501+romanbatavi@users.noreply.github.com>
2021-02-16 00:57:06 +0700

    todo dan todos di bungkus

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,35 +1,41 @@
 import React from "react";
 
+import Todo from "./Todo";
 const Todos = () => {
+  const todos = [
+    {
+      text: " Belajar1"
+    },
+    {
+      text: " Belajar2"
+    },
+    {
+      text: " Belajar3"
+    },
+    {
+      text: " Belajar4"
+    },
+    {
+      text: " Belajar5"
+    },
+    {
+      text: " Belajar6"
+    },
+    {
+      text: " Belajar7"
+    },
+    {
+      text: " Belajar8"
+    },
+    {
+      text: " Belajar9"
+    }
+  ];
   return (
     <section className="todos">
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
+      {todos.map((todo) => {
+        return <Todo text={todo.text} />;
+      })}
     </section>
   );
 };
