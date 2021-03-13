027816db8259ba11fff7385215f03c7512317eb5
Roman <73478501+romanbatavi@users.noreply.github.com>
2021-03-13 16:21:26 +0700

    layouting component container part 2

--- src/layout/Container.jsx
+++ src/layout/Container.jsx
@@ -4,15 +4,29 @@
 import { jsx, css } from "@emotion/react";
 import PropTypes from "prop-types";
 
-const Container = ({ children }) => {
+const Container = ({ children, flexDirection }) => {
   const containerStyles = css`
     display: flex;
+    flex-direction: ${flexDirection};
   `;
   return (
     <div className="flex-container" css={containerStyles}>
       {children}
     </div>
   );
+};
+
+Container.propTypes = {
+  children: PropTypes.oneOf([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  flexDirection: PropTypes.oneOf([
+    "row",
+    "row-reverse",
+    "column",
+    "column-reverse"
+  ])
 };
 
 export default Container;
