b8054591c90768aa9972071ed7e2632a0be8e03b
Roman <73478501+romanbatavi@users.noreply.github.com>
2021-03-15 15:18:25 +0700

    container todolist

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -4,6 +4,8 @@ import Paper from "../components/paper/Paper";
 import Header from "../components/header/Header";
 import TodoForm from "../components/todoform/TodoForm";
 import Todos from "../components/todos/Todos";
+import Container from "../layout/Container";
+import Item from "../layout/Item";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([
@@ -37,13 +39,15 @@ const TodoList = () => {
 
   return (
     <Paper>
-      <Header
-        showAddToggle={showAddToggle}
-        showAdd={showAdd}
-        clearTodos={clearTodos}
-      />
-      <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} completeTodo={completeTodo} />
+      <Container flexDirection="column" justifyContent="space-between">
+        <Header
+          showAddToggle={showAddToggle}
+          showAdd={showAdd}
+          clearTodos={clearTodos}
+        />
+        <TodoForm addTodo={addTodo} showAdd={showAdd} />
+        <Todos todos={todos} completeTodo={completeTodo} />
+      </Container>
     </Paper>
   );
 };
